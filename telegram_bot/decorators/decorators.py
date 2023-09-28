806a27d93dc9e888b7f4e0b2777657d1d23966cd
kovivan013 <kovivan013@gmail.com>
2023-09-29 00:52:42 +0300

    comm

--- telegram_bot/decorators/decorators.py
+++ telegram_bot/decorators/decorators.py
@@ -1,18 +1,52 @@
+import asyncio
 from functools import wraps
 from typing import Any
 from aiogram.types import Message
 from man_project_2023.telegram_bot.classes.api_requests import UserAPI
 
 
-def check_status(func):
+# def check_status(func=None, /, *, user_status=None):
+#
+#     @wraps(func)
+#     async def wrapper(*args, **kwargs) -> Any:
+#
+#         message: Message = args[1]
+#         telegram_id: int = message.from_user.id
+#         status = await UserAPI.get_user_status(telegram_id=telegram_id)
+#
+#         if status == user_status:
+#             await func(*args, kwargs)
+#
+#     return wrapper
 
-    @wraps(func)
-    async def wrapper(*args, **kwargs) -> Any:
-        message: Message = args[1]
-        telegram_id: int = message.from_user.id
-        status = await UserAPI.get_user_status(telegram_id=telegram_id)
-        if status == 0 and func.__name__ == "lost_things_menu":
-            await func(*args, **kwargs)
-        elif status == 1 and func.__name__ == "search_things_menu":
-            await func(*args, **kwargs)
-    return wrapper
+# def check_status(user_status):
+#     def decorator(func):
+#
+#         @wraps(func)
+#         async def wrapper(cls, *args, **kwargs):
+#             print(args, "||||||||", kwargs)
+#             # message: Message = args[1]
+#             telegram_id: int = 1125858430
+#             status = await UserAPI.get_user_status(telegram_id=telegram_id)
+#
+#             if status == user_status:
+#                 await func(cls, *args, kwargs)
+#
+#         return wrapper
+#     return decorator
+
+def check_status(user_status):
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(cls, *args, **kwargs):
+            # message: Message = args[1]
+            telegram_id: int = 1125858430
+            status = await UserAPI.get_user_status(telegram_id=telegram_id)
+
+            if status == user_status:
+                await func(*args, kwargs)
+
+        return wrapper
+    return decorator
+
+
--- telegram_bot/handlers/user_handlers.py
+++ telegram_bot/handlers/user_handlers.py
@@ -4,22 +4,33 @@ from aiogram.dispatcher.filters import Text
 from man_project_2023.telegram_bot.decorators.decorators import check_status
 from man_project_2023.telegram_bot.config import Dispatcher
 
+class RegisterMH:
+    pass
+
+class FinderMH: # Тот кто ищет (НАШЕЛ)
+
+    @classmethod
+    @check_status(1)
+    async def menu(cls, message: Message) -> None:
+        print(f"Меню детектива")
+
+
+class SeekerMH: # Тот кто ищет (ПОТЕРЯЛ)
+
+    @classmethod
+    @check_status(0)
+    async def menu(cls, message: Message) -> None:
+        print(f"Меню поиска")
+
+
 class StartMH:
+    finder = FinderMH()
+    seeker = SeekerMH()
 
     @classmethod
     async def menu(cls, message: Message) -> None:
-        await cls.search_things_menu(message)
-        await cls.lost_things_menu(message)
-
-    @classmethod
-    @check_status
-    async def search_things_menu(cls, message: Message) -> None:
-        await message.answer(text=f"Меню детектива")
-
-    @classmethod
-    @check_status
-    async def lost_things_menu(cls, message: Message) -> None:
-        await message.answer(text=f"Меню поиска")
+        await cls.seeker.menu(message)
+        await cls.finder.menu(message)
 
 
 def register_user_handlers(dp: Dispatcher) -> None:
